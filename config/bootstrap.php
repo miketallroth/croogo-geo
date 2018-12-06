f90188df8338b1a6d390f914233904ca5cd76565
Mike Tallroth <mike.tallroth@gmail.com>
2018-12-06 00:06:56 -0600

    Check for existence of api key

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,4 +5,6 @@ use Cake\ORM\TableRegistry;
 # migrate croogo setting to cake configuration key for cakephp-geo
 $settings = TableRegistry::get('Croogo/Settings.Settings');
 $apiKey = $settings->findByKey('Service.googlemap_key')->first();
-Configure::write('GoogleMap.key', $apiKey->value);
+if ($apiKey) {
+    Configure::write('GoogleMap.key', $apiKey->value);
+}
